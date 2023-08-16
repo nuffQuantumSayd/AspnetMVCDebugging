404ecc7f656958bac4a3906b1b3dc0a33a0dfbb9
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-08-16 16:34:01 -0700

    Added migrations and updated database

--- CPW219-AspnetMVC-CRUD-Debugging/Models/ProductContext.cs
+++ CPW219-AspnetMVC-CRUD-Debugging/Models/ProductContext.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+
 namespace CPW219_AspnetMVC_CRUD_Debugging.Models
 {
     public class ProductContext : DbContext
     {
         public ProductContext(DbContextOptions<ProductContext> options) : base(options)
         {
-            
+    
         }
+
         public DbSet<Product> Product { get; set; }
+
+        
     }
 }
